df10410a28ece4f0f71150c60a3ed6e9b3b35f1e
Killian Nthakyo <killynthakyo@gmail.com>
2023-02-12 03:13:06 -0800

    INitialise package with class dictionary

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+Creating an instance of FileStorage and calling
+ an instance of it
+"""
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+
+classes = {"BaseModel":BaseModel}
+
+storage = FileStorage()
+storage.reload()
+
