af085d896025208f8cc62469406e3fe02f77d1c1
Killian Nthakyo <killynthakyo@gmail.com>
2023-02-12 03:15:18 -0800

    Store objects to JSON file

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ Class BaseModel that defines all common attriibutes/methods for other classes
 """
 from uuid import uuid4
 from datetime import datetime
+import models
 
 
 class BaseModel():
@@ -15,30 +16,40 @@ class BaseModel():
         Initialise attributes
         """
         if (len(kwargs) != 0):
-            frm = "%Y-%m-%dT%H:%M:%S.%f"
-            kwargs["created_at"] = datetime.strptime(kwargs["created_at"], frm)
-            kwargs["updated_at"] = datetime.strptime(kwargs["updated_at"], frm)
+            form = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
-                if key != '__class__':
+                if key == "id":
+                    self.id = value
+                    continue
+                if key == "created_at":
+                    self.created_at = datetime.strptime(value, form)
+                    continue
+                if key == "updated_at":
+                    self.updated_at = datetime.strptime(value, form)
+                    continue
+                if key != "__class__":
                     setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.today()
             self.updated_at = datetime.today()
+            models.storage.new(self)
 
     def save(self):
         """
         Update the public instance attribute updated_at with current datetime
         """
         self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
         """REturn dictionary containing all keys/values"""
-        dct = self.__dict__.copy()
-        dct["__class__"] = self.__class__.__name__
-        dct["created_at"] = self.created_at.isoformat()
-        dct["updated_at"] = self.updated_at.isoformat()
-        return dct
+        cp_dct = dict(self.__dict__)
+        cp_dct['__class__'] = self.__class__.__name__
+        cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+
+        return (cp_dct)
 
     def __str__(self):
         """
